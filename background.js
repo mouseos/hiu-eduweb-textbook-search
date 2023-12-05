8b39ebc4e666fc4521b8473a8039576f8390243c
yuu <46545607+mouseos@users.noreply.github.com>
2023-12-05 15:12:34 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -1,114 +1,120 @@
 // background.js
 function getTextbook(url) {
-  return fetch(url)
-    .then(response => response.text())
-    .then(data => {
-		let htmlString=data;
-	  // JSON形式で返す
-	  return htmlString;
-    })
-    .catch(error => {
-      console.error('データの取得中にエラーが発生しました:', error);
-      return null;
-    });
+	return fetch(url)
+		.then(response => response.text())
+		.then(data => {
+			let htmlString = data;
+			// JSON形式で返す
+			return htmlString;
+		})
+		.catch(error => {
+			console.error('データの取得中にエラーが発生しました:', error);
+			return null;
+		});
 }
 
 
 function getSyllabusData(search_word, search_department, search_teacher) {
-    const url = "https://syllabus.do-johodai.ac.jp/keyword?from=title&keyword=" + search_word;
-    return fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            let htmlString = data;
+	const url = "https://syllabus.do-johodai.ac.jp/keyword?from=title&keyword=" + search_word;
+	return fetch(url)
+		.then(response => response.text())
+		.then(data => {
+			let htmlString = data;
 
-            const regex = /<tr>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td><a href="(.*?)"[^>]*>詳細<\/a><\/td>\s*<\/tr>/g;
-            let match;
-            let syllabusData;
+			const regex = /<tr>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td>(.*?)<\/td>\s*<td><a href="(.*?)"[^>]*>詳細<\/a><\/td>\s*<\/tr>/g;
+			let match;
+			let syllabusData;
 
-            while ((match = regex.exec(htmlString)) !== null) {
-                const [, lectureName, department, teacher, detailLink] = match;
+			while ((match = regex.exec(htmlString)) !== null) {
+				const [, lectureName, department, teacher, detailLink] = match;
 
-                if (department.trim().toLowerCase() === search_department.trim().toLowerCase() && teacher.trim().toLowerCase() === search_teacher.trim().toLowerCase()) {
-                    syllabusData = {
-                        '講義名': lectureName.trim(),
-                        '学科': department.trim(),
-                        '担当教員': teacher.trim(),
-                        '詳細': detailLink.trim()
-                    };
-                }
-            }
+				if (department.trim().toLowerCase() === search_department.trim().toLowerCase() && teacher.trim().toLowerCase() === search_teacher.trim().toLowerCase()) {
+					syllabusData = {
+						'講義名': lectureName.trim(),
+						'学科': department.trim(),
+						'担当教員': teacher.trim(),
+						'詳細': detailLink.trim()
+					};
+				}
+			}
 
-            if (!syllabusData) {
-                console.log(search_department + "," + search_teacher + "先生の" + search_word + "の講義は見つかりませんでした。");
-                return null;
-            }
+			if (!syllabusData) {
+				console.log(search_department + "," + search_teacher + "先生の" + search_word + "の講義は見つかりませんでした。");
+				return null;
+			}
 
-            // テキストブックの取得
-            return getTextbook(syllabusData['詳細'])
-                .then(textbookResult => {
-                    let textbook_tmp = textbookResult.split('<table class="striped">');
-                    let extractedTextbook = {};
-                    let extractedTextbooks = [];
-                    textbook_tmp.forEach(item => {
-                        if (item.includes('教科書')) {
-                            item.split("<li>").forEach(item2 => {
-                                if (item2.includes('書名')) {
-                                    item2.replace(/<[\s\S]*/g, '').trim().split(',').forEach(item3 => {
-                                        item3 = item3.trim();
-                                        if (item3.includes('書名:')) {
-                                            if (Object.keys(extractedTextbook).length !== 0) {
-                                                extractedTextbooks.push({ ...extractedTextbook }); // Add a copy of extractedTextbook
-                                                console.log("push");
-                                                console.log(extractedTextbook);
-                                                extractedTextbook = {}; // Reset extractedTextbook
-                                            }
-                                            extractedTextbook["書名"] = (item3.replace('書名:', ''));
-                                            console.log(item3.replace('書名:', ''));
-                                        } else if (item3.includes('著者名:')) {
-                                            extractedTextbook["著者名"] = (item3.replace('著者名:', ''));
-                                            console.log(item3.replace('著者名:', ''));
-                                        } else if (item3.includes('出版社:')) {
-                                            extractedTextbook["出版社"] = (item3.replace('出版社:', ''));
-                                            console.log(item3.replace('出版社:', ''));
-                                        } else if (item3.includes('備考:')) {
-                                            extractedTextbook["備考"] = (item3.replace('備考:', ''));
-                                            console.log(item3.replace('備考:', ''));
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
+			// テキストブックの取得
+			return getTextbook(syllabusData['詳細'])
+				.then(textbookResult => {
+					let textbook_tmp = textbookResult.split('<table class="striped">');
+					let extractedTextbook = {};
+					let extractedTextbooks = [];
+					textbook_tmp.forEach(item => {
+						if (item.includes('教科書')) {
+							item.split("<li>").forEach(item2 => {
+								if (item2.includes('書名')) {
+									item2.replace(/<[\s\S]*/g, '').trim().split(',').forEach(item3 => {
+										item3 = item3.trim();
+										if (item3.includes('書名:')) {
+											if (Object.keys(extractedTextbook).length !== 0) {
+												extractedTextbooks.push({
+													...extractedTextbook
+												}); // Add a copy of extractedTextbook
+												console.log("push");
+												console.log(extractedTextbook);
+												extractedTextbook = {}; // Reset extractedTextbook
+											}
+											extractedTextbook["書名"] = (item3.replace('書名:', ''));
+											console.log(item3.replace('書名:', ''));
+										} else if (item3.includes('著者名:')) {
+											extractedTextbook["著者名"] = (item3.replace('著者名:', ''));
+											console.log(item3.replace('著者名:', ''));
+										} else if (item3.includes('出版社:')) {
+											extractedTextbook["出版社"] = (item3.replace('出版社:', ''));
+											console.log(item3.replace('出版社:', ''));
+										} else if (item3.includes('備考:')) {
+											extractedTextbook["備考"] = (item3.replace('備考:', ''));
+											console.log(item3.replace('備考:', ''));
+										}
+									});
+								}
+							});
+						}
+					});
 
-                    // Add the last extractedTextbook to extractedTextbooks
-                    if (Object.keys(extractedTextbook).length !== 0) {
-                        extractedTextbooks.push({ ...extractedTextbook });
-                        console.log("push");
-                        console.log(extractedTextbook);
-                    }
+					// Add the last extractedTextbook to extractedTextbooks
+					if (Object.keys(extractedTextbook).length !== 0) {
+						extractedTextbooks.push({
+							...extractedTextbook
+						});
+						console.log("push");
+						console.log(extractedTextbook);
+					}
 
-                    syllabusData['教科書'] = extractedTextbooks;
-                    return syllabusData;
-                })
-                .catch(error => {
-                    console.error('テキストブックの取得中にエラーが発生しました:', error);
-                    return null;
-                });
-        })
-        .catch(error => {
-            console.error('データの取得中にエラーが発生しました:', error);
-            return null;
-        });
+					syllabusData['教科書'] = extractedTextbooks;
+					return syllabusData;
+				})
+				.catch(error => {
+					console.error('テキストブックの取得中にエラーが発生しました:', error);
+					return null;
+				});
+		})
+		.catch(error => {
+			console.error('データの取得中にエラーが発生しました:', error);
+			return null;
+		});
 }
 
 
-  
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'getSyllabus') {
-    getSyllabusData(request.url,request.search_department,request.search_teacher)
-      .then(data => {
-        sendResponse({ data });
-      });
-    return true;
-  }
+	if (request.action === 'getSyllabus') {
+		getSyllabusData(request.url, request.search_department, request.search_teacher)
+			.then(data => {
+				sendResponse({
+					data
+				});
+			});
+		return true;
+	}
 });
--- content.js
+++ content.js
@@ -1,117 +1,196 @@
 // content.js
 
-function SeacrhSyllabusByWord(url,search_department,search_teacher) {
-  return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage({ action: 'getSyllabus', url ,search_department,search_teacher }, response => {
-      if (response) {
-        resolve(response.data);
-      } else {
-        reject(new Error('Failed to fetch data from background'));
-      }
-    });
-  });
+function SeacrhSyllabusByWord(url, search_department, search_teacher) {
+	return new Promise((resolve, reject) => {
+		chrome.runtime.sendMessage({
+			action: 'getSyllabus',
+			url,
+			search_department,
+			search_teacher
+		}, response => {
+			if (response) {
+				resolve(response.data);
+			} else {
+				reject(new Error('Failed to fetch data from background'));
+			}
+		});
+	});
 }
 
 // ページの読み込みが完了した後に実行する関数
 function main() {
 	/*==============HTMLから時間割を取得する==============*/
 	//学部名
-	let department=document.querySelectorAll(".pull-right.student-info-header > div > span")[3].innerText.replace("所属:","").trim();
+	let department = document.querySelectorAll(".pull-right.student-info-header > div > span")[3].innerText.replace("所属:", "").trim();
 	//console.log("学部："+department);
-	let week_table=document.querySelectorAll("#week-table-tbody")
+	let week_table = document.querySelectorAll("#week-table-tbody")
 	console.log(week_table);
 	//時間割保管連想配列
-	let timetable={};
+	let timetable = {};
 	//学期を保存
-	let cnt_term=0;
+	let cnt_term = 0;
 	let current_week;
 	let subject;
 	let teacher;
 	week_table.forEach((table) => {
-		timetable[cnt_term]={};
+		timetable[cnt_term] = {};
 		//console.log(table.children);
-		for (let i = 0; i < table.children.length ; i++){
-		  let day_table=(table.children[i].children);
-		  
+		for (let i = 0; i < table.children.length; i++) {
+			let day_table = (table.children[i].children);
 
-		  
-		  
-		  for (let j=0; j<day_table.length; j++){
-			  //時間割内文字列
-			  let timetable_str=(day_table[j].innerHTML);
-			  //console.log("timetable_str="+timetable_str);
-			  /*console.log("j="+j);
-			  console.log("i="+i);
-			  console.log("cnt_term="+cnt_term);*/
-			  if(j==0){
-				  timetable[cnt_term][timetable_str]={};
-				  current_week=timetable_str;
-			  }
-			  //授業名と教員名に分ける
-			  if(timetable_str.length!=0){
-				  let timetable_str_split=(timetable_str.split("<br>"));
-				  //console.log(timetable_str_split);
 
-				subject=timetable_str_split[0];
-				teacher=timetable_str_split[1];
-				subject=(subject.trim());
-				//teacherがない場合
-				if(typeof(teacher)=='string'){
-					teacher=(teacher.trim());
+
+
+			for (let j = 0; j < day_table.length; j++) {
+				//時間割内文字列
+				let timetable_str = (day_table[j].innerHTML);
+				//console.log("timetable_str="+timetable_str);
+				/*console.log("j="+j);
+				console.log("i="+i);
+				console.log("cnt_term="+cnt_term);*/
+				if (j == 0) {
+					timetable[cnt_term][timetable_str] = {};
+					current_week = timetable_str;
 				}
-			  }else{
-				  teacher="";
-				  subject="";
-			  }
-				if(j!=0){
-					timetable[cnt_term][current_week][j-1]={subject : subject,teacher: teacher};
+				//授業名と教員名に分ける
+				if (timetable_str.length != 0) {
+					let timetable_str_split = (timetable_str.split("<br>"));
+					//console.log(timetable_str_split);
+
+					subject = timetable_str_split[0];
+					teacher = timetable_str_split[1];
+					subject = (subject.trim());
+					//teacherがない場合
+					if (typeof(teacher) == 'string') {
+						teacher = (teacher.trim());
+					}
+				} else {
+					teacher = "";
+					subject = "";
 				}
-		  }
+				if (j != 0) {
+					timetable[cnt_term][current_week][j - 1] = {
+						subject: subject,
+						teacher: teacher
+					};
+				}
+			}
 		}
 		cnt_term++;
 	});
 	/*==============HTMLから時間割を取得する==============*/
-	/*==============時間割下部に時間割表示==============*/
-	document.querySelector("#tab1").innerHTML+="<h4>購入教科書一覧</h4><p>ここに書く</p>";
-	/*==============時間割下部に時間割表示==============*/
+
 	/*==============シラバスを検索する==============*/
-	let search_department=department;
-	let search_teacher="";
+	let search_department = department;
+	let search_teacher = "";
 
+	// 全てのプロミスを待つための配列
+	let textbooks_tmp = new Set();
+	let terms = ["前期前半", "前期後半", "後期前半", "後期後半"];
+	for (const term in timetable) {
+		for (const week in timetable[term]) {
+			for (const day in timetable[term][week]) {
+				const {
+					subject,
+					teacher
+				} = timetable[term][week][day];
+				if (subject && teacher) {
+					// 重複をチェックしてから追加
+					const searchText = `${subject}_${teacher}`;
+					if (!textbooks_tmp.has(searchText)) {
+						textbooks_tmp.add(searchText); // Setに追加
+					}
+				}
+			}
+		}
+	}
 
+	// Setから配列に変換してPromise.allを実行する
+	Promise.all(Array.from(textbooks_tmp).map(searchText => {
+			const [subject, teacher] = searchText.split('_');
+			return SeacrhSyllabusByWord(subject, department, teacher);
+		}))
+		.then(textbooks => {
+			let filteredTextbooks = textbooks.filter(data => data !== null);
+			console.log(filteredTextbooks);
+			document.querySelector("#tab1").innerHTML += "<h4>購入教科書一覧</h4>";
 
+			const tableHeader = `<thead class="sticky-top">
+                          <tr>
+                              <th>講義名</th>
+                              <th>担当教員</th>
+                              <th>教科書名</th>
+                              <th>著者名</th>
+                              <th>出版社</th>
+                              <th>備考</th>
+                              <th>購入</th>
+                          </tr>
+                      </thead>`;
 
+			let tableBody = '<tbody>';
+			console.log(filteredTextbooks);
+			filteredTextbooks.forEach((textbook) => {
+				if (textbook && textbook["講義名"] && textbook["担当教員"] && textbook["教科書"] && textbook["教科書"][0]) {
+					const {
+						講義名,
+						担当教員,
+						教科書
+					} = textbook;
+					const {
+						書名,
+						著者名,
+						出版社,
+						備考
+					} = 教科書[0];
 
+					tableBody += `<tr>
+                              <td>${講義名 ? 講義名 : ''}</td>
+                              <td>${担当教員 ? 担当教員 : ''}</td>
+                              <td>${書名 ? 書名 : ''}</td>
+                              <td>${著者名 ? 著者名 : ''}</td>
+                              <td>${出版社 ? 出版社 : ''}</td>
+                              <td>${備考 ? 備考 : ''}</td>
+                              <!--紀伊国屋書店 amazon メルカリ paypayフリマ ラクマ ヤフオクで検索-->
+                              <td class="shops text-nowrap">
+                                  <!--紀伊国屋書店はログインしないと検索できない。そのため、クリップボードに書名をコピーしてからhttps://mykits.kinokuniya.co.jp/Login/37002cfd-c010-4ae5-bcc4-e94f9a3fa48bに飛ばす-->
+                                  <ul>
+                                  <li><a data="${書名} ${著者名}" href="https://mykits.kinokuniya.co.jp/Login/37002cfd-c010-4ae5-bcc4-e94f9a3fa48b" target="_blank">紀伊国屋書店</a></li>
+                                  <li><a href="https://www.amazon.co.jp/s?k=${書名 ? 書名 : ''}+${著者名 ? 著者名 : ''}" target="_blank">amazon</a></li>
+                                  <li><a href="https://www.mercari.com/jp/search/?keyword=${書名 ? 書名 : ''}+${著者名 ? 著者名 : ''}" target="_blank">メルカリ</a></li>
+                                  <li><a href="https://paypayfleamarket.yahoo.co.jp/search/${書名 ? 書名 : ''}+${著者名 ? 著者名 : ''}" target="_blank">paypayフリマ</a></li>
+                                  <li><a href="https://fril.jp/s?query=${書名 ? 書名 : ''}+${著者名 ? 著者名 : ''}" target="_blank">ラクマ</a></li>
+                                  <li><a href="https://auctions.yahoo.co.jp/search/search?p=${書名 ? 書名 : ''}+${著者名 ? 著者名 : ''}" target="_blank">ヤフオク</a></li>
+                                  </ul>
+                              </td>      
+                        </tr>`;
+				}
+			});
 
+			tableBody += '</tbody>';
 
+			const tableHTML = `<table class="table table-striped">${tableHeader}${tableBody}</table>`;
+			document.querySelector("#tab1").innerHTML += tableHTML;
+			console.log('完了');
+			const elements = document.querySelectorAll('a[data]');
 
+			elements.forEach(function(element) {
+				element.addEventListener('click', async function(event) {
+					event.preventDefault();
 
+					const content = this.getAttribute('data');
 
-
-
-// SeacrhSyllabusByWord関数を呼び出す前に、timetableの内容を検索して渡す
-for (const term in timetable) {
-    for (const week in timetable[term]) {
-      for (const day in timetable[term][week]) {
-        const { subject, teacher } = timetable[term][week][day];
-        if (subject && teacher) {
-          SeacrhSyllabusByWord(subject, department, teacher)
-            .then(data => {
-              console.log(data);
-              // ここで取得したdataを使って何かを行う（必要に応じて処理を追加）
-            })
-            .catch(error => {
-              console.error('データの取得中にエラーが発生しました:', error);
-            });
-        }
-      }
-    }
-  }
-
-
-
-
-
+					try {
+						await navigator.clipboard.writeText(content);
+						window.open(this.href, '_blank');
+					} catch (error) {
+						console.error('クリップボードへのコピーに失敗しました:', error);
+					}
+				});
+			});
+		})
+		.catch(error => {
+			console.error('エラーが発生しました:', error);
+		});
 
 	/*==============シラバスを検索する==============*/
 }
